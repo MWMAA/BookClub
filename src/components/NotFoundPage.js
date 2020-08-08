c821048ce6607d759bec7b79497a39369eec226b
MWMAA <mahmoudamer95.bird@gmail.com>
2020-08-08 11:23:44 +0200

    master clearerd

--- src/components/NotFoundPage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-export default () => (
-  <div>
-    link not found! Go to Dashboard <Link to={'/'} >Dashboard</Link>
-  </div>
-)
